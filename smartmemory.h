9dc7859a455cbf1a89fe14daa7f91b256e087870
tanimoto <04girl.u.want@gmail.com>
2020-12-23 23:30:28 +0900

    スマートポインタの宣言をまとめることができた。

--- smartmemory.h
+++ smartmemory.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <memory>
 
-#define SMART_PTR_IMPL(x) using x ## _up = std::unique_ptr< x ##  > ; \
-                          using x ## _sp = std::shared_ptr< x ## > ; \
-                          using x ## _wp = std::weak_ptr< x ## > ;
+#define SMART_PTR_IMPL(x) using x ## _up = std::unique_ptr< x > ; \
+                          using x ## _sp = std::shared_ptr< x > ; \
+                          using x ## _wp = std::weak_ptr< x > ;
