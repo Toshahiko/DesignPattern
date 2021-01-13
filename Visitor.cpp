21199b5f611e9d1431787a50f8db72372b1523f1
tanimoto <04girl.u.want@gmail.com>
2021-01-13 22:04:13 +0900

    フォローアップ研修の課題用に編集した。

--- Visitor.cpp
+++ Visitor.cpp
@@ -2,23 +2,20 @@
 #include <iostream>
 #include <vector>
 #include <memory>
-class Visitor ;
+#include <algorithm>
+#include <numeric>
 
-class Element {
+class Entry {
 public:
-  virtual void Accept( const Visitor& visitor ) = 0 ;
-} ;
-
-class Entry : public Element {
-public:
-  virtual std::string GetName() const noexcept ;
-  virtual int GetSize() const noexcept ;
-  virtual void Add( const std::shared_ptr<Entry> entry ) = 0 ;
+  virtual std::string GetName() const noexcept = 0 ;
+  virtual int GetSize() const noexcept = 0 ;
+  virtual void Add( const std::shared_ptr<Entry>& entry ) {} ;
 } ;
 
 class File : public Entry {
 public:
-  void Accept( const Visitor& visitor ) override ;
+  File( const std::string& name, int size )
+  : m_name( name ), m_size( size ) {}
   std::string GetName() const noexcept final { return m_name ; }
   int GetSize() const noexcept final { return m_size ; }
 private:
@@ -28,26 +25,37 @@ private:
 
 class Directory : public Entry {
 public:
-  void Accept( const Visitor& visitor ) override ;
-  std::string GetName() const noexcept final ;
-  int GetSize() const noexcept final ;
-  void Add( const std::shared_ptr<Entry> entry ) override ;
+  Directory( const std::string& name )
+  : m_name( name ) {}
+
+  std::string GetName() const noexcept final {
+    return m_name ;
+  }
+
+  int GetSize() const noexcept final {
+    return std::accumulate( m_entries.cbegin(), m_entries.cend(), 0,
+                            []( int result, const std::shared_ptr<Entry>& entry ) { return entry->GetSize() + result ; } ) ;
+  }
+
+  void Add( const std::shared_ptr<Entry>& entry ) override {
+    if ( entry.get() == this ) {
+      std::cout << "error" << std::endl ;
+      return ;
+    }
+    auto it = std::find( m_entries.cbegin(), m_entries.cend(), entry ) ;
+    if ( it != m_entries.cend() ) {
+      std::cout << "error" << std::endl ;
+      return ;
+    }
+    m_entries.push_back( entry ) ;
+  }
+
 private:
   std::string m_name ;
-  std::vector<std::shared_ptr<Entry>> m_dir ;
-} ;
-
-class Visitor {
-public:
-  virtual void Visit( const File& file ) = 0 ;
-  virtual void Visit( const Directory& file ) = 0 ;
-} ;
-
-class ListVisitor : public Visitor {
-  void Visit( const File& file ) override ;
-  void Visit( const Directory& directory ) override ;
+  std::vector<std::shared_ptr<Entry>> m_entries ;
 } ;
 
 int main() {
-  return 0 ;
+
+  
 }
