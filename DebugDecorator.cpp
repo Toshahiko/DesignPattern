c1dbbac905c1d5c5ef6b7b1fedc19bedde12cfc5
tanimoto <04girl.u.want@gmail.com>
2021-01-23 22:24:01 +0900

    Decoratorパターン終了

--- DebugDecorator.cpp
+++ DebugDecorator.cpp
@@ -27,8 +27,33 @@ auto decorate_debug( const Callable& c, const char* s ) {
   return DebugDecorator<Callable>( c, s ) ;
 }
 
+template<typename Callable>
+class LockDecorator {
+public:
+  LockDecorator( const Callable& c, std::mutex& m )
+    : m_c( c ), m_m( m ) {}
+
+  template<typename ... Args>
+  auto operator()( Args&& ... args ) const {
+    std::lock_guard<std::mutex> l( m_m ) ;
+    return m_c( std::forward<Args>( args ) ... ) ;
+  }
+private:
+  const Callable& m_c ;
+  std::mutex& m_m ;
+} ;
+
+template <typename Callable>
+auto decorate_lock( const Callable& c, std::mutex& m ) {
+  return LockDecorator<Callable>( c, m ) ;
+}
+
 int Plus( int a, int b ) {
   return a + b ;
+}
+
+int unsafe_f( int x ) {
+  return x ;
 }
 
 } // anonymous
@@ -41,4 +66,9 @@ int main() {
   auto deb1 = decorate_debug( []( int& x ) { return x ; }, "x" ) ;
   int x = 4 ;
   deb1( x ) ;
+
+  std::mutex m ;
+  auto safe_f = decorate_debug( decorate_lock( [] ( int x ) { return unsafe_f( x ) ; }, m ), "f(x) " ) ;
+  safe_f( 1 ) ;
+
 }
