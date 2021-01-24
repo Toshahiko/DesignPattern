be037bd6bee443bbf9b7e5f18e627865ebe99b33
tanimoto <04girl.u.want@gmail.com>
2021-01-24 13:34:54 +0900

    Double Dispatchを実装

--- Visitor_opp.cpp
+++ Visitor_opp.cpp
@@ -2,20 +2,22 @@
 namespace {
 class Base {
 public:
-  virtual void f() = 0 ;
+  virtual void f( Base* p ) = 0 ;
 } ;
 
 class Derived1 : public Base {
-  void f() override {};
+  void f( Base* p ) override {
+    // p->f( this ) ;
+  };
 } ;
 
 class Derived2 : public Base {
-  void f() override {};
+  void f( Base* p ) override {};
 } ;
 
 } // anonymous namespace
 
 int main() {
   Base* b = new Derived1 ;
-  b->f() ; // Single Dispatch。呼ばれる関数が一つのファクターで決まる。この場合はオブジェクトの実際の型。
+  b->f( b ) ; // Single Dispatch。呼ばれる関数が一つのファクターで決まる。この場合はオブジェクトの実際の型。
 }
