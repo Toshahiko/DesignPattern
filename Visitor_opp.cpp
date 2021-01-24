876ef573fc0c323c8167af507a5cf6492c8e19e3
tanimoto <04girl.u.want@gmail.com>
2021-01-24 14:12:09 +0900

    Visitorパターンの初期実装

--- Visitor_opp.cpp
+++ Visitor_opp.cpp
@@ -1,23 +1,68 @@
+#include <string>
+#include <iostream>
+#include <memory>
 
 namespace {
-class Base {
+class PetVisitor ;
+
+class Pet {
 public:
-  virtual void f( Base* p ) = 0 ;
+  virtual ~Pet() = default ;
+  Pet( const std::string& color ) : m_color( color ) {}
+  const std::string& Color() const { return m_color ; }
+  virtual void Accept( PetVisitor& v ) = 0 ;
+private:
+  std::string m_color ;
 } ;
 
-class Derived1 : public Base {
-  void f( Base* p ) override {
-    // p->f( this ) ;
-  };
+class Cat ;
+class Dog ;
+
+class PetVisitor {
+  public:
+  virtual ~PetVisitor() = default ;
+  virtual void Visit( Cat* c ) = 0 ;
+  virtual void Visit( Dog* d ) = 0 ;
 } ;
 
-class Derived2 : public Base {
-  void f( Base* p ) override {};
+class Cat : public Pet {
+  using Pet::Pet ;
+  public:
+  void Accept( PetVisitor& v ) override { v.Visit( this ) ; }
+} ;
+
+class Dog : public Pet {
+  using Pet::Pet ;
+  public:
+  void Accept( PetVisitor& v ) override { v.Visit( this ) ; }
+} ;
+
+class FeedingVisitor : public PetVisitor {
+  public:
+  void Visit( Cat* c) override {
+    std::cout << "Feed tuna to the " << c->Color() << " cat " << std::endl ;
+  }
+  void Visit( Dog* d) override {
+    std::cout << "Feed steak to the " << d->Color() << " dog " << std::endl ;
+  }
+
+} ;
+
+class PlayingVisitor : public PetVisitor {
+  public:
+  void Visit( Cat* c) override {
+    std::cout << "Play with feather with the " << c->Color() << " cat " << std::endl ;
+  }
+  void Visit( Dog* d) override {
+    std::cout << "Play fetch with the " << d->Color() << " dog " << std::endl ;
+  }
+
 } ;
 
 } // anonymous namespace
 
 int main() {
-  Base* b = new Derived1 ;
-  b->f( b ) ; // Single Dispatch。呼ばれる関数が一つのファクターで決まる。この場合はオブジェクトの実際の型。
+  std::unique_ptr<Pet> c = std::make_unique<Cat>( "orange" ) ;
+  std::unique_ptr<PetVisitor> fv = std::make_unique<FeedingVisitor>() ;
+  c->Accept( *fv ) ;
 }
